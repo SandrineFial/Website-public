4c8dc20755d4a5fbbdff9d6d08c05125fe333edb
Sandrine <fialon.sandrine@gmail.com>
2023-06-07 20:00:00 +0200

    Ajoute google trak

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,11 @@ import React from "react";
 import Head from 'next/head';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-
+/*
+import ReactGA from 'react-ga';
+const TRACKING_ID = "UA-XXXXX-X"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
+*/
 function App({ Component, pageProps }) {
 
   return (
