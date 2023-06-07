73d854ab4c43504508bc8b1d1df2622224d7ba2a
Sandrine <fialon.sandrine@gmail.com>
2023-06-07 19:48:59 +0200

    Test form contact

--- components/Contact.js
+++ components/Contact.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Helmet } from "react-helmet";
 import Image from 'next/image';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone, faEnvelope, faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { useForm } from 'react-hook-form';
+import emailjs from '@emailjs/browser';
+
 /*
 import { ApiClient, Configuration, TransactionalEmailsApi } from 'sib-api-v3-sdk';
 const defaultClient = ApiClient.instance;
@@ -35,139 +37,32 @@ export default function Contact() {
   const metaDescription = "Vous souhaitez renforcez votre équipe pour la création et mise à jour de vos sites internet, ainsi que de vos applications web et mobiles ? Un devis ou d'autres informations, je me ferais un plaisir de vous répondre. Je suis disponible sur Marseille, Aix-en-Provence et communes limitrophes ainsi qu'à distance."
   const metaTitle = "Contact - Freelance - développeur web - application mobile";
 
-  const envoiMail = async (e) => {
+  const envoiMail = (e) => {
+    console.log('Coucou formulaire');
     if(nomMsg!=="" && mailMsg !=="" && textMsg!=="") {
-      /*
-      const sendSmtpEmail = {
-          to: [{ "email": "fialon.sandrine@gmail.com", "name": "To Name" }],
-          sender: { "email": "from@example.com", "name": "From Name" },
-          subject: "Subject of the message",
-          htmlContent: "<html><body><h1>Hi!</h1><p>Test email sent from React App using Sendinblue API.</p></body></html>"
-      };
-      transactionalEmailsApi.sendTransacEmail(sendSmtpEmail).then(function(data) {
-        console.log('API called successfully. Returned data: ' + JSON.stringify(data));
-    }, function(error) {
-        console.error(error);
-    });*/
-/*
-      apiInstance.sendTransacEmail({
-        "subject":"Test Subject",
-        "sender":{
-            "name":"John Doe",
-            "email":"johndoe@example.com"
-        },
-        "to":[
-            {
-                "email":"jane.doe@example.com",
-                "name":"Jane Doe"
-            }
-        ],
-        "htmlContent":"<html><body><h1>Hello, World!</h1></body></html>"
-      })
-      .then((data) => {
-        console.log('API called successfully. Returned data: ' + data);
-      }, (error) => {
-        console.error(error);
-      });
-*/
-      /*
-      var defaultClient = SibApiV3Sdk.ApiClient.instance;
+      console.log('Champs non vide');
+        e.preventDefault(); 
+        const templateParams = {
+          user_name: nomMsg,
+          user_email: mailMsg,
+          message: textMsg
+        };
 
-      // Configure API key authorization: api-key
-      var apiKey = defaultClient.authentications['api-key'];
-      apiKey.apiKey = API_KEY
-      // Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
-      //apiKey.apiKeyPrefix['api-key'] = "Token"
+        emailjs .sendForm( 
+          process.env.REACT_APP_SERVICE_ID, 
+          process.env.REACT_APP_TEMPLATE_ID, 
+          templateParams, 
+          process.env.REACT_APP_PUBLIC_KEY ) 
+        .then( (result) => { 
+          console.log("Message envoyé...", result.text); 
+        }, 
+        (error) => { 
+            console.log("Non envoyé", error.text); } 
+        ); 
 
-      // Configure API key authorization: partner-key
-      var partnerKey = defaultClient.authentications['partner-key'];
-      partnerKey.apiKey = API_KEY
-      // Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
-      //partnerKey.apiKeyPrefix['partner-key'] = "Token"
-
-      var api = new SibApiV3Sdk.AccountApi()
-      api.getAccount().then(function(data) {
-        console.log('API called successfully. Returned data: ' + data);
-      }, function(error) {
-        console.error(error);
-      }); */
-      /*
-      var defaultClient = SibApiV3Sdk.ApiClient.instance;
-      
-      var apiKey = defaultClient.authentications['api-key'];
-      apiKey.apiKey = API_KEY;
-      var apiInstance = new SibApiV3Sdk.EmailCampaignsApi();
-      var emailCampaigns = new SibApiV3Sdk.CreateEmailCampaign();
-            
-      emailCampaigns.name = "Campaign sent via the API";
-      emailCampaigns.subject = "Web Message Freelance";
-      emailCampaigns.sender = {"name": "From name", "email": mailMsg};
-      emailCampaigns.type = "classic";
-      // Content that will be sent\
-      emailCampaigns.htmlContent = textMsg;
-      // Select the recipients\
-      // Make the call to the client\
-      apiInstance.createEmailCampaign(emailCampaigns).then(function(data) {
-        console.log('API called successfully. Returned data: ' + data);
-      }, function(error) {
-      console.error(error);
-      });
-*/
-    } else {
-
-    }
-    /*
-    await sendEmail({
-      to: "kiran@example.com",
-      subject: "Welcome to NextAPI",
-      html: render(WelcomeTemplate()),
-    });
-    return res.status(200).json({ message: "Email sent successfully" });
-    */
-    /*
-    e.preventDefault();
-
-    axios.post('/send-email', form)
-      .then((res) => {
-        console.log(res);
-        setForm({ ...form, [e.target.name]: '' });
-      })
-      .catch((err) => console.log(err));
-      */
-/*
-      // Generate test SMTP service account from ethereal.email
-      // Only needed if you don't have a real mail account for testing
-      let testAccount = await nodemailer.createTestAccount();
-    
-      // create reusable transporter object using the default SMTP transport
-      let transporter = nodemailer.createTransport({
-        host: "smtp.ethereal.email",
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-          user: testAccount.user, // generated ethereal user
-          pass: testAccount.pass, // generated ethereal password
-        },
-      });
-    
-      // send mail with defined transport object
-      let info = await transporter.sendMail({
-        from: nomMsg+' <'+mailMsg+'>', // sender address
-        to: "archestia@gmail.com", // list of receivers
-        subject: "Message site Webdev Freelance", // Subject line
-        text: textMsg, // plain text body
-      });
-    
-      console.log("Message sent: %s", info.messageId);
-      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-    
-      // Preview only available when sending through an Ethereal account
-      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-      // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-    */
-
+    } else { }
   }
-
+  
   return (
       <div className='md:w-3/4 mx-auto'>
         
@@ -208,15 +103,18 @@ export default function Contact() {
           <p>N'hésitez pas à me contacter pour un projet commun dans le web.<br/>
             Je ne manquerais pas de vous répondre.</p>
         </div>
-        {/*  
-        <form onSubmit={ () => envoiMail }>
+        
+        <form onSubmit={envoiMail}>
             <div className="p-5 mx-auto mb-10 rounded-lg container bg-slate-100 max-w-xs md:max-w-md">
                 <div className='mb-3 flex flex-col justify-center items-center'>
-                  <input type="text" placeholder="Nom prénom" className="input input-bordered input-md w-full max-w-md mb-5"
+                  <input type="text" placeholder="Nom prénom" name='user_name'
+                    className="input input-bordered input-md w-full max-w-md mb-5"
                     onChange={(e) => setnomMsg(e.target.value)} value={nomMsg} required />
-                  <input type="email" placeholder="E-mail" className="input input-bordered input-md w-full max-w-md mb-5"
+                  <input type="email" placeholder="E-mail" name='user_email'
+                    className="input input-bordered input-md w-full max-w-md mb-5"
                     onChange={(e) => setmailMsg(e.target.value)} value={mailMsg} required />
-                  <textarea placeholder="Message" className="textarea textarea-bordered textarea-md w-full max-w-md mb-5"
+                  <textarea placeholder="Message" name='message'
+                  className="textarea textarea-bordered textarea-md w-full max-w-md mb-5"
                   onChange={(e) => settextMsg(e.target.value)} value={textMsg} required ></textarea>
                   <div>
                     <button className="bg-sky-200 rounded-lg px-10 p-3 shadow hover:bg-sky-600 hover:text-white cursor-pointer flex justify-center items-center">
@@ -227,7 +125,7 @@ export default function Contact() {
                 </div>
             </div>
           </form>
-        */}
+        
       </div>
       
   );
--- components/Footer.js
+++ components/Footer.js
@@ -10,24 +10,19 @@ function Footer() {
     <footer className="footer footer-center p-4 bg-sky-950 text-neutral-content">
       <div className="items-center grid-flow-col">
         
-        <Link href="/">
-          <div className='md:block hidden'>
-            <Image src="/logo.png" alt="Logo" width={70} height={50} />
-          </div>
-        </Link>
-        <p>Copyright © 2023 - S. Fialon</p>
-      </div> 
-      <div className="grid-flow-col gap-4">
-        <div className='w-7 h-7 cursor-pointer'>
+        <div className='w-6 h-3 cursor-pointer mr-2'>
           <Link href="https://www.linkedin.com/in/fialonsandrine/">
               <FontAwesomeIcon icon={ faLinkedinIn }/>
           </Link>
         </div>
-        <div className='w-7 h-7 cursor-pointer'>
+        <div className='w-6 h-3 cursor-pointer'>
           <Link href="tel:0683874742">
               <FontAwesomeIcon icon={ faPhone }/>
           </Link>
         </div>
+      </div> 
+      <div className="grid-flow-col gap-4">
+        <p>Copyright © 2023 - S. Fialon</p>
       </div>
     </footer>
 
