ac03fb0a06f58c8bb961dc891bc161f7630396f9
Sandrine <fialon.sandrine@gmail.com>
2023-06-08 15:35:31 +0200

    Intégration Google Analytics ga4

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,23 @@
 //require('dotenv').config();
-import styles from '../styles/globals.css';
 import React from "react";
+import styles from '../styles/globals.css';
 import Head from 'next/head';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-/*
-import ReactGA from 'react-ga';
-const TRACKING_ID = "G-WPZ58PNGBF"; //"351786095"; //  // OUR_TRACKING_ID
-ReactGA.initialize(TRACKING_ID);
-ReactGA.pageview(window.location.pathname);
-*/
-function App({ Component, pageProps }) {
+import ReactGA from "react-ga4";
 
+function App({ Component, pageProps }) {
+  
+  if (typeof window !== "undefined") {
+    const TRACKING_ID = "G-WPZ58PNGBF"; //"351786095"; //  // OUR_TRACKING_ID
+    ReactGA.initialize(TRACKING_ID);
+    const splitLocation = window.location.pathname;
+    ReactGA.send({
+      hitType: "pageview",
+      page: splitLocation,
+    });
+  }
+  
   return (
     <>
     <Head>
